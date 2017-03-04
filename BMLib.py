a96f1dabcf0f43a784e44a68dcd803156578f167
IKEDA, Ritsuya <cyb01263@halt9000.org>
2017-03-04 22:32:02 +0900

    Add create faces select feature.

--- BMLib.py
+++ BMLib.py
@@ -6,24 +6,63 @@ IDEAL_NOTCH_WIDTH = 4
 
 
 def genFrontPoints(w, h, d, t, faces):
-    return chain(
-        genHorizontalLinePoints(0, t, w, -t, t),
-        genVerticalLinePoints(w - t, 0, h, t, t),
-        genHorizontalLinePoints(w, h - t, -w, t, -t),
-        genVerticalLinePoints(t, h, -h, -t, -t),
-    )
+    offset = t if faces.left else 0
+    if faces.bottom:
+        l1 = genHorizontalLinePoints(0, t, w, -t, offset)
+        offset = t
+    else:
+        len = w - (0 if faces.left else t) -  (0 if faces.right else t)
+        l1 = genHorizontalStraightLinePoints(0, 0, len,  offset)
+        offset = 0
+    if faces.right:
+        l2 = genVerticalLinePoints(w - t, 0, h, t, offset)
+        offset = t
+    else:
+        l2 = genVerticalStraightLinePoints(w, 0, h, offset)
+        offset = 0
+    if faces.top:
+        l3 = genHorizontalLinePoints(w, h - t, -w, t, -offset)
+        offset = t
+    else:
+        len = w - (0 if faces.right else t) -  (0 if faces.left else t)
+        l3 = genHorizontalStraightLinePoints(w, h, -len, -offset)
+        offset = 0
+    if faces.left:
+        l4 = genVerticalLinePoints(t, h, -h, -t, -offset)
+    else:
+        len = w - (0 if faces.top else t) -  (0 if faces.bottom else t)
+        l4 = genVerticalStraightLinePoints(0, h, -len, -offset)
+
+    return chain(l1, l2, l3, l4)
 
 def genBackPoints(w, h, d, t, faces):
     return genFrontPoints(w, h, d, t, faces)
 
 
 def genLeftPoints(w, h, d, t, faces):
-    return chain(
-        genHorizontalLinePoints(0, t, -d, -t, 0),
-        genVerticalLinePoints(-d, 0, h, t, t),
-        genHorizontalLinePoints(-d, h - t, d, t, 0),
-        genVerticalLinePoints(0, h, -h, -t, -t),
-    )
+    if faces.bottom:
+        l1 = genHorizontalLinePoints(0, t, -d, -t, 0)
+        offset = t
+    else:
+        l1 = genHorizontalStraightLinePoints(0, 0, -d, 0)
+        offset = 0
+    if faces.front:
+        l2 = genVerticalLinePoints(-d, 0, h, t, offset)
+    else:
+        len = w - (0 if faces.bottom else t) -  (0 if faces.top else t)
+        l2 = genVerticalStraightLinePoints(-d, 0, len, offset)
+    if faces.top:
+        l3 = genHorizontalLinePoints(-d, h - t, d, t, 0)
+        offset = t
+    else:
+        l3 =  genHorizontalStraightLinePoints(-d, h, d, 0)
+        offset = 0
+    if faces.back:
+        l4 = genVerticalLinePoints(0, h, -h, -t, -offset)
+    else:
+        len = w - (0 if faces.top else t) -  (0 if faces.bottom else t)
+        l4 = genVerticalStraightLinePoints(0, h, -len, -offset)
+    return chain(l1, l2, l3, l4)
 
 
 def genRightPoints(w, h, d, t, faces):
@@ -35,12 +74,23 @@ def genBottomPoints(w, h, d, t, faces):
 
 
 def genTopPoints(w, h, d, t, faces):
-    return chain(
-        genHorizontalLinePoints(0, 0, w, -t, 0),
-        genVerticalLinePoints(w, 0, -d, -t, 0),
-        genHorizontalLinePoints(w, -d, -w, t, 0),
-        genVerticalLinePoints(0, -d, d, t, 0),
-    )
+    if faces.back:
+        l1 = genHorizontalLinePoints(0, 0, w, -t, 0)
+    else:
+        l1 = genHorizontalStraightLinePoints(0, 0, -w, 0)
+    if faces.right:
+        l2 = genVerticalLinePoints(w, 0, -d, -t, 0)
+    else:
+        l2 = genVerticalStraightLinePoints(w, 0, -d, 0)
+    if faces.front:
+        l3 = genHorizontalLinePoints(w, -d, -w, t, 0)
+    else:
+        l3 = genHorizontalStraightLinePoints(w, -d, -w, 0)
+    if faces.left:
+        l4 = genVerticalLinePoints(0, -d, d, t, 0)
+    else:
+        l4 = genVerticalStraightLinePoints(0, -d, d, 0)
+    return chain(l1, l2, l3, l4)
 
 
 def genHorizontalLinePoints(x, y, length, notchHeight, offset):
@@ -68,5 +118,18 @@ def genHorizontalLinePoints(x, y, length, notchHeight, offset):
 def genVerticalLinePoints(x, y, length, notchHeight, offset):
     # Symmetrical with the horizontal version, but with x & y swapped
     points = genHorizontalLinePoints(y, x, length, notchHeight, offset)
+    for y, x in points:
+        yield (x, y)
+
+
+def genHorizontalStraightLinePoints(x, y, length, offset):
+    x = float(x)
+    y = float(y)
+    yield (x + offset, y)
+    yield (x + length - offset, y)
+
+
+def genVerticalStraightLinePoints(x, y, length, offset):
+    points = genHorizontalStraightLinePoints(y, x, length, offset)
     for y, x in points:
         yield (x, y)
